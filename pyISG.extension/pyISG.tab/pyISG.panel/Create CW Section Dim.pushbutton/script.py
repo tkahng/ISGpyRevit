a4797bf492289b737cf8b400b30585e6fe129733
tkahng <tkahng@gmail.com>
2020-02-15 05:40:57 +0900

    a

--- pyISG.extension/pyISG.tab/pyISG.panel/Create CW Section Dim.pushbutton/script.py
+++ pyISG.extension/pyISG.tab/pyISG.panel/Create CW Section Dim.pushbutton/script.py
@@ -46,12 +46,15 @@ def create_section(wall, section_type):
     section_box.Min = bbox_min
     section_box.Max = bbox_max
 
-    DB.ViewSection.CreateSection(revit.doc, section_type, section_box)
+    return DB.ViewSection.CreateSection(revit.doc, section_type, section_box)
 
 
 def get_walls():
     """retrieve wall from selection set"""
     return [x for x in revit.get_selection() if isinstance(x, DB.Wall)]
+
+def isParallel(v1,v2):
+  return v1.CrossProduct(v2).IsAlmostEqualTo(DB.XYZ(0,0,0))
 
 
 def get_section_viewfamily():
@@ -60,8 +63,51 @@ def get_section_viewfamily():
         )
 
 
+
+
 doc_section_type = get_section_viewfamily()
 
 with revit.Transaction("Create Parallel Section"):
     for selected_wall in get_walls():
+        dimline = selected_wall.Location.Curve
         create_section(selected_wall, doc_section_type)
+
+
+def createdim(wall, line, view):
+    line = wall.Location.Curve
+    lineDir = line.GetEndPoint(1) - line.GetEndPoint(0)
+
+    refArray = DB.ReferenceArray()
+
+    # doc = DocumentManager.Instance.CurrentDBDocument
+
+    options = DB.Options()
+    options.ComputeReferences = True
+    options.IncludeNonVisibleObjects = True
+
+    geoElement = wall.get_Geometry(options)
+
+    #get side references
+    for obj in geoElement:
+        if isinstance(obj,DB.Solid):
+            for f in obj.Faces:
+                faceNormal = f.FaceNormal
+                if isParallel(faceNormal,lineDir):
+                    refArray.Append(f.Reference)
+                    
+                    
+    #get grid references
+    for id in wall.CurtainGrid.GetVGridLineIds():
+        gridLine = revit.doc.GetElement(id)
+        gridGeo = gridLine.get_Geometry(options)
+        for obj in gridGeo:
+            if isinstance(obj,DB.Line):
+                refArray.Append(obj.Reference)
+
+
+    # TransactionManager.Instance.EnsureInTransaction(doc)
+    revit.doc.Create.NewDimension(view, line, refArray)
+    # TransactionManager.Instance.TransactionTaskDone()
+
+    #Assign your output to the OUT variable.
+    # OUT = 0
