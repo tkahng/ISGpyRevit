5e5c874553b6497e8c522ba71099787f71085a77
tkahng <tkahng@gmail.com>
2020-02-15 07:31:57 +0900

    a

--- pyISG.extension/pyISG.tab/pyISG.panel/Batch Create Parallel Section.pushbutton/script.py
+++ pyISG.extension/pyISG.tab/pyISG.panel/Batch Create Parallel Section.pushbutton/script.py
@@ -12,46 +12,56 @@ output = script.get_output()
 
 def create_section(wall, section_type):
     # ensure wall is straight
-    line = wall.Location.Curve
+    curve = wall.Location.Curve
     # determine section box
-    p = line.GetEndPoint(0)
-    q = line.GetEndPoint(1)
-    v = q - p
+    curve_transform = curve.ComputeDerivatives(0.5, True)
 
-    bb = wall.get_BoundingBox(None)
-    minZ = bb.Min.Z
-    maxZ = bb.Max.Z
-
-    w = v.GetLength()
-    # h = maxZ - minZ
-    # d = wall.WallType.Width
-    offset = 0.1 * w
-
-    bbox_min = DB.XYZ(-w, minZ - offset, -offset)
-    bbox_max = DB.XYZ(w, maxZ + offset, 0)
-
-    midpoint = p + 0.5 * v
-    walldir = v.Normalize()
+    origin = curve_transform.Origin
+    wall_direction = curve_transform.BasisX.Normalize()  # type: XYZ
     up = DB.XYZ.BasisZ
-    viewdir = walldir.CrossProduct(up)
+    section_direction = wall_direction.CrossProduct(up)
+    right = up.CrossProduct(section_direction)
 
-    t = DB.Transform.Identity
-    t.Origin = midpoint
-    t.BasisX = walldir
-    t.BasisY = up
-    t.BasisZ = viewdir
+    transform = DB.Transform.Identity
+    transform.Origin = origin
+    transform.BasisX = wall_direction
+    transform.BasisY = up
+    transform.BasisZ = section_direction
 
     section_box = DB.BoundingBoxXYZ()
-    section_box.Transform = t
-    section_box.Min = bbox_min
-    section_box.Max = bbox_max
+    section_box.Transform = transform
 
-    DB.ViewSection.CreateSection(revit.doc, section_type, section_box)
+    # Try to retrieve element height, width and lenght
+    try:
+        el_depth =  wall.WallType.Width
+    except AttributeError:
+        el_depth = 2
+
+    el_width = curve.Length
+
+    el_bounding_box = wall.get_BoundingBox(None)
+    z_min = el_bounding_box.Min.Z
+    z_max = el_bounding_box.Max.Z
+    el_height = z_max - z_min
+
+    depth_offset = 1
+    height_offset = 1
+    width_offset = 1
+
+    # Set BoundingBoxXYZ's boundaries
+    section_box.Min = DB.XYZ(-el_width / 2 - width_offset,
+                          -height_offset,
+                          -el_depth / 2 - depth_offset)
+    section_box.Max = DB.XYZ(el_width / 2 + width_offset,
+                          el_height + height_offset,
+                          el_depth / 2 + depth_offset)
+
+    return DB.ViewSection.CreateSection(revit.doc, section_type, section_box)
 
 
-def get_walls():
-    """retrieve wall from selection set"""
-    return [x for x in revit.get_selection() if isinstance(x, DB.Wall)]
+# def get_walls():
+#     """retrieve wall from selection set"""
+#     return [x for x in revit.get_selection() if isinstance(x, DB.Wall)]
 
 def get_cw():
     wc = DB.FilteredElementCollector(revit.doc)\
@@ -59,16 +69,58 @@ def get_cw():
                     .WhereElementIsNotElementType()\
                     .ToElements()
     return [w for w in wc if w.WallType.Kind.ToString() == 'Curtain']
-    
+
+def isParallel(v1,v2):
+  return v1.CrossProduct(v2).IsAlmostEqualTo(DB.XYZ(0,0,0))    
 
 def get_section_viewfamily():
     return revit.doc.GetDefaultElementTypeId(
         DB.ElementTypeGroup.ViewTypeSection
         )
 
+def create_dim(wall, line, view):
+    line = wall.Location.Curve
+    lineDir = line.GetEndPoint(1) - line.GetEndPoint(0)
+
+    refArray = DB.ReferenceArray()
+
+    # doc = DocumentManager.Instance.CurrentDBDocument
+
+    options = DB.Options()
+    options.ComputeReferences = True
+    options.IncludeNonVisibleObjects = True
+
+    geoElement = wall.get_Geometry(options)
+
+    #get side references
+    for obj in geoElement:
+        if isinstance(obj,DB.Solid):
+            for f in obj.Faces:
+                faceNormal = f.FaceNormal
+                if isParallel(faceNormal,lineDir):
+                    refArray.Append(f.Reference)
+                    
+                    
+    #get grid references
+    for id in wall.CurtainGrid.GetVGridLineIds():
+        gridLine = revit.doc.GetElement(id)
+        gridGeo = gridLine.get_Geometry(options)
+        for obj in gridGeo:
+            if isinstance(obj,DB.Line):
+                refArray.Append(obj.Reference)
+
+
+    # TransactionManager.Instance.EnsureInTransaction(doc)
+    revit.doc.Create.NewDimension(view, line, refArray)
+    # TransactionManager.Instance.TransactionTaskDone()
+
+    #Assign your output to the OUT variable.
+    # OUT = 0
 
 doc_section_type = get_section_viewfamily()
 
 with revit.Transaction("Create Parallel Section"):
     for selected_wall in get_cw():
-        create_section(selected_wall, doc_section_type)
+        dimline = selected_wall.Location.Curve
+        sec = create_section(selected_wall, doc_section_type)
+        create_dim(selected_wall, dimline, sec)
