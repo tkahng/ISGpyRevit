b227066c6268668659433342a19403ba6853639c
tkahng <tkahng@gmail.com>
2020-02-15 17:43:01 +0900

    a

--- pyISG.extension/pyISG.tab/pyISG.panel/Create CW Dim.pushbutton/script.py
+++ pyISG.extension/pyISG.tab/pyISG.panel/Create CW Dim.pushbutton/script.py
@@ -9,69 +9,12 @@ __author__ = 'Source: Jeremy Tammik\nAdapted by: {{author}}'
 logger = script.get_logger()
 output = script.get_output()
 
-
-def create_section(wall, section_type):
-    # ensure wall is straight
-    line = wall.Location.Curve
-    # determine section box
-    p = line.GetEndPoint(0)
-    q = line.GetEndPoint(1)
-    v = q - p
-
-    bb = wall.get_BoundingBox(None)
-    minZ = bb.Min.Z
-    maxZ = bb.Max.Z
-
-    w = v.GetLength()
-    # h = maxZ - minZ
-    # d = wall.WallType.Width
-    offset = 0.1 * w
-
-    bbox_min = DB.XYZ(-w, minZ - offset, -offset)
-    bbox_max = DB.XYZ(w, maxZ + offset, 0)
-
-    midpoint = p + 0.5 * v
-    walldir = v.Normalize()
-    up = DB.XYZ.BasisZ
-    viewdir = walldir.CrossProduct(up)
-
-    t = DB.Transform.Identity
-    t.Origin = midpoint
-    t.BasisX = walldir
-    t.BasisY = up
-    t.BasisZ = viewdir
-
-    section_box = DB.BoundingBoxXYZ()
-    section_box.Transform = t
-    section_box.Min = bbox_min
-    section_box.Max = bbox_max
-
-    return DB.ViewSection.CreateSection(revit.doc, section_type, section_box)
-
-
 def get_walls():
     """retrieve wall from selection set"""
     return [x for x in revit.get_selection() if isinstance(x, DB.Wall)]
 
 def isParallel(v1,v2):
   return v1.CrossProduct(v2).IsAlmostEqualTo(DB.XYZ(0,0,0))
-
-
-def get_section_viewfamily():
-    return revit.doc.GetDefaultElementTypeId(
-        DB.ElementTypeGroup.ViewTypeSection
-        )
-
-
-
-
-doc_section_type = get_section_viewfamily()
-
-with revit.Transaction("Create Parallel Section"):
-    for selected_wall in get_walls():
-        dimline = selected_wall.Location.Curve
-        create_section(selected_wall, doc_section_type)
-
 
 def createdim(wall, line, view):
     line = wall.Location.Curve
@@ -111,3 +54,8 @@ def createdim(wall, line, view):
 
     #Assign your output to the OUT variable.
     # OUT = 0
+
+with revit.Transaction("Create Parallel Section"):
+    for selected_wall in get_walls():
+        dimline = selected_wall.Location.Curve
+        create_section(selected_wall, doc_section_type)
