6376ec1d9586a86323eb3c9ca834f6b54b8fa535
tkahng <tkahng@gmail.com>
2020-08-15 04:34:29 +0900

    a

--- pyISG.extension/pyISG.tab/pyISG.panel/FinishWallByRoom.pushbutton/script.py
+++ pyISG.extension/pyISG.tab/pyISG.panel/FinishWallByRoom.pushbutton/script.py
@@ -7,7 +7,7 @@ clr.AddReference('RevitAPI')
 
 import rpw
 from rpw import doc, uidoc
-from rpw.ui.forms import ComboBox, Button, Label, FlexForm, TextBox
+from rpw.ui.forms import ComboBox, Button, Label, FlexForm, TextBox, CheckBox
 import Autodesk
 from Autodesk.Revit.DB import *
 from Autodesk.Revit.UI import Selection
@@ -51,19 +51,20 @@ def curveloop_from_boundary(boundary):
 
 
 def form():
-    # floor_types = rpw.db.Collector(of_category='OST_Floors', is_type=True,
-    #                               where=lambda x: x.GetParameters('Thickness')).get_elements()
+    floor_types = rpw.db.Collector(of_category='OST_Floors', is_type=True).get_elements()
     wall_types = rpw.db.Collector(of_category='Walls', is_type=True,
                                   where=lambda x: x.GetParameters('Width')).get_elements()
     components = [
-                    # Label('Finish floor type:'),
-                    # ComboBox('floor_type_id',
-                    #         {DB.Element.Name.GetValue(ft): ft for ft in floor_types}),
+                    Label('Finish floor type:'),
+                    ComboBox('floor_type_id',
+                            {ft.parameters['Type Name'].AsString(): ft.Id for ft in floor_types}),
+                    # Label('No Floors'),
+                    CheckBox('make_floors', 'make Floors'),
                     Label('Finish wall type:'),
                     ComboBox('wall_type_id',
                             {wt.parameters['Type Name'].AsString(): wt.Id for wt in wall_types}),
                     Label('Finish wall height (mm):'),
-                    TextBox('wall_height'),
+                    TextBox('wall_height', default='3000'),
                     Button('Create Finish Walls')
                   ]
 
@@ -71,10 +72,11 @@ def form():
     ff.show()
     if ff.values['wall_type_id'] and ff.values['wall_height']:
         try:
-            # floor_type = rpw.db.Element(ff.values['floor_type_id'])
+            floor_type = rpw.db.Element.from_id(ff.values['floor_type_id'])
             wall_type = rpw.db.Element.from_id(ff.values['wall_type_id'])
             wall_height = float(ff.values['wall_height'])
-            return wall_type, wall_height
+            make_floors = ff.values['make_floors']
+            return wall_type, wall_height, floor_type, make_floors
         except:
             return
 
@@ -89,27 +91,29 @@ def is_inside_room(curveloop, room):
     return True
 
 
-# @rpw.db.Transaction.ensure('Make Floor')
-# def make_floor(floor_type_id, boundary, level_id):
-#     floor_curves = DB.CurveArray()
-#     for boundary_segment in boundary:
-#         try:
-#             floor_curves.Append(boundary_segment.Curve)       # 2015, dep 2016
-#         except AttributeError:
-#             floor_curves.Append(boundary_segment.GetCurve())  # 2017
+@rpw.db.Transaction.ensure('Make Floor')
+def make_floor(floor_type, boundary, level_id):
+    floor_curves = CurveArray()
+    for boundary_segment in boundary:
+        try:
+            floor_curves.Append(boundary_segment.Curve)       # 2015, dep 2016
+        except AttributeError:
+            floor_curves.Append(boundary_segment.GetCurve())  # 2017
 
-#     floorType = doc.GetElement(floor_type_id)
-
-#     level = doc.GetElement(level_id)
-#     normal_plane = DB.XYZ.BasisZ
-#     doc.Create.NewFloor(floor_curves, floorType, level, False, normal_plane)
-
+    floorType = doc.GetElement(floor_type)
+    offset = rpw.db.Element(floorType).parameters['Default Thickness'].AsDouble()
+    level = doc.GetElement(level_id)
+    normal_plane = XYZ.BasisZ
+    floor = doc.Create.NewFloor(floor_curves, floorType, level, False, normal_plane)
+    rpw.db.Element(floor).parameters['Height Offset From Level'] = offset
+    return floor
 
 # @rpw.db.Transaction.ensure('create finish walls')
-def create_finish_wall(room, wall_type, wall_height):
+def create_finish_wall(room, wall_type, wall_height, floor_type, make_floors):
     offset_distance = wall_type.parameters['Width'].AsDouble() * 0.5
     boundary_loops = get_boundaries(room)
-    # new_floors = make_floor(floor_type.Id, boundary_loops, room.LevelId)
+    if make_floors:
+        new_floor = make_floor(floor_type.Id, boundary_loops[0], room.LevelId)
     # print(boundary_loops)
     for boundary in boundary_loops:
         curveloop = curveloop_from_boundary(boundary)
@@ -120,7 +124,7 @@ def create_finish_wall(room, wall_type, wall_height):
             offset_curveloop = CurveLoop.CreateViaOffset(curveloop, -offset_distance,
                                                          curveloop.GetPlane().Normal)
         new_walls = []
-        # new_floors = make_floor(floor_type.Id, )
+        # new_floors = make_floor(floor_type.Id, curveloop, room.LevelId)
         # with rpw.db.TransactionGroup('run def'):
 
         with rpw.db.Transaction('Create Finish Wall'):
@@ -140,36 +144,28 @@ def create_finish_wall(room, wall_type, wall_height):
                         JoinGeometryUtils.JoinGeometry(doc, old_wall, new_wall)
                     except Exception as e:
                         print(e)
+
+        with rpw.db.Transaction('Join floor-new walls'):
+            for idx, new_wall in enumerate(new_walls):
+                if make_floors:
+                    try:
+                        JoinGeometryUtils.JoinGeometry(doc, new_floor, new_wall)
+                    except Exception as e:
+                        print(e)
+
         with rpw.db.Transaction('Delete short walls'):
             for new_wall in new_walls:
                 length = new_wall.LookupParameter('Length').AsDouble() * 304.8
                 if length < 50:
                     doc.Delete(new_wall.Id)
 
-        # with rpw.db.Transaction('Create finish floor'):
-
-        #     for curve in offset_curveloop:
-        #         new_wall = Wall.Create(doc, curve, wall_type.Id, room.LevelId,
-        #                             wall_height/304.8, 0, False, False)
-        #         new_walls.append(new_wall)
-
-
-# try:
-#     wall_type, wall_height = form()
-#     rooms = select_objects_by_category('Rooms')
-#     for room in rooms:
-#         create_finish_wall(room, wall_type, wall_height)
-# except Exception as e:
-#     print(e)
-
-
 def main():
     with rpw.db.TransactionGroup('run def'):
         try:
-            wall_type, wall_height = form()
+            wall_type, wall_height, floor_type, make_floors = form()
             rooms = select_objects_by_category('Rooms')
             for room in rooms:
-                create_finish_wall(room, wall_type, wall_height)
+                create_finish_wall(room, wall_type, wall_height, floor_type, make_floors)
         except Exception as e:
             print(e)
 
